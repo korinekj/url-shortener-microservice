d0a69b9fd75038ec54aded3ba5aba60f30b7e2cb
Jarda Korinek <korinek14.j@gmail.com>
2022-08-23 12:36:33 +0200

    added funtionality POST url to api/shortcut -> check with regex if url is valid -> server response json with original url or json with error message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
@@ -8,17 +8,38 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+//zajistí že můžu využít payload (form data), respektive req.body níže...nevím nerozumím tomu pořádně zatímpičo
+app.use(express.urlencoded());
+
+app.post("/api/shorturl", function (req, res) {
+  const validUrlRegex =
+    /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+
+  const isValidUrl = validUrlRegex.test(req.body.url);
+
+  if (isValidUrl) {
+    res.json({
+      original_url: req.body.url,
+      short_url: "zde short url",
+    });
+  } else {
+    res.json({
+      error: "invalid url",
+    });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
